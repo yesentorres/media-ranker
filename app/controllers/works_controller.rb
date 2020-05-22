df7d78b96e63ae15eb00430dd4374752bfb9bd79
Yesenia <yesentorres@gmail.com>
2020-05-21 19:03:39 -0700

    updated works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,80 @@
 class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @work = Work.new
+  end 
+
+  def create
+    @work = Work.new(work_params) 
+
+    if @work.save
+      flash[:success] = "Successfully created #{@work.category} #{@work.id}"
+      redirect_to work_path(@work.id)
+      return
+    else 
+      flash.now[:error] = "A problem occured: Could not create #{@work.category}"
+      render :new 
+      return
+    end
+  end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      flash[:success] = "Successfully updated #{@work.category} #{@work.id}"
+      redirect_to work_path(@work.id)
+      return
+    else 
+      flash.now[:error] = "A problem occured: Could not update #{@work.category}"
+      render :edit 
+      return
+    end
+  end
+
+  def destroy
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    end 
+
+    @work.destroy
+    flash[:success] = "Successfully destroyed #{@work.category} #{@work.id}"
+    redirect_to root_path
+    return
+  end
+
+end
+
+private
+
+def work_params
+  return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
 end
