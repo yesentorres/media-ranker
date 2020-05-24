1d9020f21061d7b7c8fcbaf82398e98d3ea74dd4
Yesenia <yesentorres@gmail.com>
2020-05-23 21:41:07 -0700

    added brute force solutions to main page

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,5 +2,26 @@ class Work < ApplicationRecord
   validates :title, presence: true
   validates_uniqueness_of :title, scope: :category
 
-  has_many :votes
+  has_many :votes, dependent: :destroy
+
+  def sort(category)
+    # assumption: we can expect a user will never assign a work to a cateogry other than the ones listed below 
+    # since work and edit views use the select element to pick a category
+
+    works_list = Works.all 
+
+    case category 
+      when "album"
+        albums = works_list.select { |work| work.category == "album" }
+        return albums
+      when "book"
+        books = works_list.select { |work| work.category == "book" }
+        return books  
+      when "movie"
+        movies = works_list.select { |work| work.category == "movie" }
+        return movies
+      end 
+
+  end
+
 end
