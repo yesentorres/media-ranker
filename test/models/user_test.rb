7f3c546aa7d67b1c7d499a60b25e4cb8b3754322
Yesenia <yesentorres@gmail.com>
2020-05-24 23:38:53 -0700

    added vote tests, updated user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,7 +30,13 @@ describe User do
       v1 = Vote.create(user_id: users(:toby).id, work_id: works(:thewall).id)
       v2 = Vote.create(user_id: users(:toby).id, work_id: works(:ratatouille).id)
 
+      # Act
+      result1 = v1.valid?
+      result2 = v2.valid?
+
       # Assert
+      expect(result1).must_equal true
+      expect(result2).must_equal true
       expect(users(:toby).votes.length).must_equal 2
     end
   end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,81 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe 'validations' do
+    it "can be created with a valid user id and valid work id" do
+      # Arrange 
+      toby = users(:toby)
+      thewall = works(:thewall)
+      vote = Vote.create(user_id: toby.id, work_id: thewall.id)
+
+      # Act
+      result = vote.valid?
+
+      # Assert 
+      expect(result).must_equal true
+    end 
+
+    it "cannot be created with an invalid user id" do
+      # Arrange 
+      thewall = works(:thewall)
+      vote = Vote.create(user_id: -1, work_id: thewall.id)
+
+      # Act
+      result = vote.valid?
+
+      # Assert 
+      expect(result).must_equal false
+    end
+
+    it "cannot be created with an invalid work id" do
+      # Arrange 
+      toby = users(:toby)
+      vote = Vote.create(user_id: toby.id, work_id: -1)
+
+      # Act
+      result = vote.valid?
+
+      # Assert 
+      expect(result).must_equal false
+    end
+
+    it "cannot be created when user has already voted for the work" do 
+      # Arrange 
+      toby = users(:toby)
+      thewall = works(:thewall)
+      vote1 = Vote.create(user_id: toby.id, work_id: thewall.id)
+      vote2 = Vote.create(user_id: toby.id, work_id: thewall.id)
+
+      # Act
+      result = vote2.valid?
+
+      # Assert 
+      expect(result).must_equal false
+      expect(vote2.errors.messages).must_include :user
+      expect(vote2.errors.messages[:user]).must_include "has already voted for this work"
+    end 
+  end
+
+  describe 'relations' do
+    it "belongs to a user" do 
+      # Arrange
+      toby = users(:toby)
+      thewall = works(:thewall)
+      vote = Vote.create(user_id: toby.id, work_id: thewall.id)
+
+      # Act - Assert
+      expect(toby.votes.first.id).must_equal vote.id
+    end 
+
+    it "belongs to a work" do 
+      # Arrange
+      toby = users(:toby)
+      thewall = works(:thewall)
+      vote = Vote.create(user_id: toby.id, work_id: thewall.id)
+
+      # Act - Assert
+      expect(thewall.votes.first.id).must_equal vote.id
+    end 
+  end
 end
